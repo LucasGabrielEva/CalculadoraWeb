1f73984ca05bb3be4d1484605f28909bfbaf9592
Lucas Gabriel <lucasgabriel19672@gmail.com>
2026-05-13 10:10:21 -0300

    Delete Calculadora_web directory

--- Calculadora_web/src/main/java/com/example/Calculadora_web/repository/OperacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Calculadora_web.repository;
-
-import com.example.Calculadora_web.model.Operacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OperacaoRepository extends JpaRepository<Operacao,Long> {
-}
